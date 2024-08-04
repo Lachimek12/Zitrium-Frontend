69817d1747ace99809856bf7274f5603c1b1b09c
Lachimek12 <michal_szkola20@wp.pl>
2024-08-04 17:06:41 +0200

    Update to work with the latest backend version

--- src/components/pages/VerifyEmail.tsx
+++ src/components/pages/VerifyEmail.tsx
@@ -4,6 +4,7 @@ import { useEffect} from "react";
 /* App modules imports */
 // import { SERVER_ADDRESS } from "@utils/constants";
 import { useFetch } from "@/hooks/useFetch";
+import { usePostFetch } from "@/hooks/useFetch";
 // import { DataTransfer } from "../../types/ApiResponse";
 import UserData from "@customTypes/UserData";
 import { DataTransfer } from "@/types/ApiResponse";
@@ -14,10 +15,15 @@ import { useLocation } from "react-router-dom";
 function VerifyEmail() {
   const query = new URLSearchParams(useLocation().search);
   const token: string | null = query.get("token");
-  const response: DataTransfer<UserData> = useFetch<UserData>(`http://localhost:3000/verify-email?token=${token}`);
+  //const response: DataTransfer<UserData> = useFetch<UserData>(`http://localhost:3000/verify-email?token=${token}&email=michal_szkola20@wp.pl`);
+  const data = {
+    email: 'michal_szkola20@wp.pl',
+    token: token,
+  }
+  const sendToken = usePostFetch(data, 'http://localhost:3000/verify-email');
 
   useEffect(() => {
-    response.request();
+    sendToken.request();
   }, []);
 
   return (
